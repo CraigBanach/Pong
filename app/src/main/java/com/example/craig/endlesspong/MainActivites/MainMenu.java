3dfe4bf6871a5063331088863cb4b78457e4610a
Craig Banach <craigsb93@gmail.com>
2015-11-14 21:51:25 +0000

    Added main menu buttons - Videoview still not working correctly.

--- app/src/main/java/com/example/craig/endlesspong/MainActivites/MainMenu.java
+++ app/src/main/java/com/example/craig/endlesspong/MainActivites/MainMenu.java
@@ -1,10 +1,17 @@
-package com.example.craig.endlesspong;
+package com.example.craig.endlesspong.MainActivites;
 
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import com.example.craig.endlesspong.R;
+
+import org.w3c.dom.Text;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -14,18 +21,23 @@ public class MainMenu extends AppCompatActivity {
 
     setContentView(R.layout.activity_main_menu);
 
-    VideoView mainVideoView = (VideoView) findViewById(R.id.mainPageVideoView);
+    initializeViews();
 
-    MediaPlayer mainVideoPlayer = MediaPlayer.create(this, +R.raw.documentariesandyou);
-
-    mainVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.documentariesandyou));
-
-
-    mainVideoView.setZOrderMediaOverlay(true);
-    mainVideoView.setZOrderOnTop(true);
-
-    mainVideoView.start();
   }
+
+  private void initializeViews() {
+    // Initialize Videoview, find source and start playback.
+    VideoView mainVideoView = (VideoView) findViewById(R.id.mainPageVideoView);
+    mainVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.documentariesandyou));
+    mainVideoView.setZOrderOnTop(true);
+    mainVideoView.start();
+
+    // Initialize the mainTextView and set the font.
+    TextView nameTitleScreen = (TextView) findViewById(R.id.nameTitleScreen);
+    Typeface type = Typeface.createFromAsset(getAssets(),"fonts/CostaRica.ttf");
+    nameTitleScreen.setTypeface(type);
+  }
+
   protected void onPostCreate(Bundle savedInstanceState) {
     super.onPostCreate(savedInstanceState);
 
