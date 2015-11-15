f33e4edfd26eb7eea3a2472db2373bc207a19dda
Craig Banach <craigsb93@gmail.com>
2015-11-15 21:47:05 +0000

    Added single player screen and drawables.

--- app/src/main/java/com/example/craig/endlesspong/game/OnePlayer.java
+++ app/src/main/java/com/example/craig/endlesspong/game/OnePlayer.java
@@ -1,15 +1,22 @@
 package com.example.craig.endlesspong.game;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.craig.endlesspong.R;
 
 /**
  * This activity sets up the game for a single palyer, before passing to the main game activity.
  * Created by craig on 15/11/2015.
  */
-public class OnePlayer {
+public class OnePlayer extends AppCompatActivity {
 
   protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.oneplayer);
+
+
 
   }
 }
